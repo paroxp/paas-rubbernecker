b252a65137949e5c0cf5ccac7911815c5b9cc804
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2017-01-30 14:02:36 +0000

    Implement state auto changes on the front end
    In order, not to be out of date, we'd like to implement a little check,
    that would be running in the background of the application and compare
    the tokens provided of the content obtained from Pivotal.
    
    If the change is detected, we should get a little notice, mentioning
    that, with a countdown giving us chance to cancel or manually refresh
    the page.
    
    Option, to control auto-update has been added into option bar.

--- public/js/rubbernecker.js
+++ public/js/rubbernecker.js
@@ -1,4 +1,58 @@
 var App = {
+    config: {
+        updateCheck: null,
+        updatePending: null,
+        countdowns: []
+    },
+
+    checkForUpdates: function () {
+        if (App.config.updatePending !== null) {
+            return;
+        }
+
+        $.ajax({
+            url: "/",
+            type: "GET",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('x-rubbernecker-state', currentState);
+            },
+            success: function (data, textStatus, xhr) {
+                if (data.state === 'dirty') {
+                    $('.update').slideDown();
+                    App.config.updatePending = setTimeout(App.refresh, 30 * 1000); // Refresh page in about 30 seconds.
+                    App.setupCountdown('state', 30, '.update p span');
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('Failed to obtain the update response.');
+                console.debug(jqXHR, textStatus, errorThrown);
+            }
+        });
+    },
+
+    disableUpdates: function () {
+        if (App.config.updateCheck !== null) {
+            clearInterval(App.config.updateCheck);
+            App.config.updateCheck = null;
+        }
+
+        if (App.config.updatePending !== null) {
+            clearTimeout(App.config.updatePending);
+            App.config.updatePending = null;
+        }
+
+        $('.board-updates [data-switch="on"]').removeClass('active');
+        $('.board-updates [data-switch="off"]').addClass('active');
+
+        $('.update').slideUp();
+    },
+
+    enableUpdates: function (seconds) {
+        if (App.config.updateCheck === null) {
+            App.config.updateCheck = setInterval(App.checkForUpdates, seconds * 1000);
+        }
+    },
+
     gracefulIn: function ($elements) {
         $elements.each(function () {
             var $element = $(this);
@@ -37,6 +91,26 @@ var App = {
         });
     },
 
+    refresh: function () {
+        location.reload();
+    },
+
+    setupCountdown: function (name, seconds, element) {
+        $(element).attr('data-seconds', seconds).text(seconds);
+
+        clearInterval(App.config.countdowns[name]);
+
+        App.config.countdowns[name] = setInterval(function () {
+            var left = $(element).attr('data-seconds') - 1;
+
+            if (left < 0) {
+                return;
+            }
+
+            $(element).attr('data-seconds', left).text(left);
+        }, 1000);
+    },
+
     toggleMenu: function (e) {
         e.preventDefault();
 
@@ -46,11 +120,24 @@ var App = {
     },
 
     toggleCards: function (e) {
+        App.toggleSwitch(e);
+
+        var target = $(this).attr('data-target'),
+            toHide = $(this).attr('data-hide');
+
+        // Show all the stories.
+        App.gracefulIn($(target));
+
+        // Hide other elements if possible.
+        if (toHide) {
+            App.gracefulOut($(target).filter(toHide));
+        }
+    },
+
+    toggleSwitch: function (e) {
         e.preventDefault();
 
-        var $switches = $(this).parent().find('a[data-switch]'),
-            target = $(this).attr('data-target'),
-            toHide = $(this).attr('data-hide');
+        var $switches = $(this).parent().find('a[data-switch]');
 
         if ($(this).hasClass('active')) {
             return;
@@ -60,14 +147,6 @@ var App = {
         $switches.filter('.active').removeClass('active');
         $(this).addClass('active');
 
-        // Show all the stories.
-        App.gracefulIn($(target));
-
-        // Hide other elements if possible.
-        if (toHide) {
-            App.gracefulOut($(target).filter(toHide));
-        }
-
         return false;
     }
 };
@@ -75,6 +154,18 @@ var App = {
 $(document)
     .ready(function () {
         $('body')
+            .on('click', '[data-switch]', App.toggleSwitch)
             .on('click', '.team-switch a', App.toggleCards)
-            .on('click', 'div.options .handler', App.toggleMenu);
+            .on('click', 'div.options .handler', App.toggleMenu)
+            .on('click', '.update [data-trigger="cancel"], .board-updates [data-switch="off"]', App.disableUpdates)
+            .on('click', '.board-updates [data-switch="on"]', setupUpdates)
+            .on('click', '.update [data-trigger="refresh"]', App.refresh);
+
+        setupUpdates();
+
+        //////////
+
+        function setupUpdates() {
+            App.enableUpdates(30); // Enable updates every 30 seconds.
+        }
     });
